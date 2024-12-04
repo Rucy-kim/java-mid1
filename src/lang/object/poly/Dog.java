be00347d38b2af2249a1471bb5e9ebd338ba9806
Rucy-kim <khj2xx@gmail.com>
2024-12-04 18:17:23 +0900

    1. Object 클래스

---
+++ src/lang/object/poly/Dog.java
@@ -0,0 +1,12 @@
+package lang.object.poly;
+
+public class Dog {
+    public void sound() {
+        System.out.println("멍멍");
+    }
+
+    @Override
+    public String toString() {
+        return "멍멍";
+    }
+}
